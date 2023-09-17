0f0ae6dd4461e231696ce1a82b7176c879cb17f2
quickjkee <askjfasokf@askdjasoi.ee>
2023-09-18 01:26:33 +0300

    first

--- consistency_models-sd/runner.py
+++ consistency_models-sd/runner.py
@@ -21,10 +21,9 @@ OUTPUT_PATH = get_blob_logdir()
 torch.hub.load('facebookresearch/dinov2', 'dinov2_vitl14')
 
 for step in [6]:
-    for ref_step in [5]: #5, 10, 15, 25, 35, 45
-        for rollback_v in [0.1, 0.12, 0.13, 0.14, 0.15, 0.16, 0.17, 0.18, 0.19, 0.2,
-                           0.21, 0.22, 0.23, 0.24, 0.25, 0.26, 0.27, 0.28, 0.29, 0.3,
-                           0.31, 0.32, 0.33, 0.34, 0.35, 0.4]:
+    for ref_step in [15]: #5, 10, 15, 25, 35, 45
+        for rollback_v in [0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5,
+                           0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95]:
             print(f'GENERATION WITH CD STEPS {step}, REF STEPS {ref_step}, ROLLBACK V {rollback_v}')
             subprocess.call(f'CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.run --standalone \
                              --nproc_per_node=8 --master-addr=0.0.0.0:1207 scripts/cm_train.py \
