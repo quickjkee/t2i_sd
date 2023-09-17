b08aa41a708faa169f903b82c2054455a2e4b10b
quickjkee <askjfasokf@askdjasoi.ee>
2023-09-17 22:08:56 +0300

    first

--- consistency_models-sd/cm/train_util.py
+++ consistency_models-sd/cm/train_util.py
@@ -451,7 +451,7 @@ class CMTrainLoop(TrainLoop):
 
         # Setup seed equalt ot the world rank
         #timesteps = th.tensor([981, 669, 505, 280, 272,   1], device='cuda')  # extremely hard code
-        logger.log(f'CURRENT TIMESTEPS {timesteps}')
+        #logger.log(f'CURRENT TIMESTEPS {timesteps}')
 
         if num_refining_steps:
             logger.log(f'Refining with rollback value {rollback_value} and steps {num_refining_steps}')
