d9aaea6d3d626bc26f99f9d8445923b36a16a94f
quickjkee <askjfasokf@askdjasoi.ee>
2023-09-19 16:22:01 +0300

    first

--- consistency_models-sd/runner.py
+++ consistency_models-sd/runner.py
@@ -23,7 +23,7 @@ OUTPUT_PATH = get_blob_logdir()
 
 for step in [6]:
     for ref_step in [10]: #5, 10, 15, 25, 35, 45
-        for rollback_v in [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]:
+        for rollback_v in [0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75]:
             print(f'GENERATION WITH CD STEPS {step}, REF STEPS {ref_step}, ROLLBACK V {rollback_v}')
             subprocess.call(f'CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python3 -m torch.distributed.run --standalone \
                              --nproc_per_node=8 --master-addr=0.0.0.0:1207 scripts/cm_train.py \
