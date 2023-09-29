7a265530c1b2de7b36be4f28adcea4b2587ec7ba
quickjkee <askjfasokf@askdjasoi.ee>
2023-09-29 23:15:53 +0300

    first

--- consistency_models-sd/cm/diffusion.py
+++ consistency_models-sd/cm/diffusion.py
@@ -197,7 +197,9 @@ class DenoiserSD:
             # latents_prev = self.scheduler_step(teacher_noise_pred, t, t2, latents)
             latents_prev = []
             for j, pred in enumerate(teacher_noise_pred):
-                latents_prev.append(self.pipe.scheduler.step(pred, t[j], latents[j], self.generator, False)[0])
+                curr_p = self.pipe.scheduler.step(pred, t[j], latents[j], self.generator, False)[0]
+                print(curr_p.size())
+                latents_prev.append(curr_p)
             latents_prev = torch.stack(latents_prev)
 
             if self.use_fp16:
