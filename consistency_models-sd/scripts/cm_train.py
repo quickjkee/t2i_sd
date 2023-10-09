68dbd40d756f2dac29cc0c0863a37a12bc2eb4ac
quickjkee <askjfasokf@askdjasoi.ee>
2023-10-10 02:43:40 +0300

    first

--- consistency_models-sd/scripts/cm_train.py
+++ consistency_models-sd/scripts/cm_train.py
@@ -51,7 +51,7 @@ def prepare_coco_prompts(path, bs=25, max_cnt=10000):
 
     num_batches = ((len(all_text) - 1) // (bs * dist.get_world_size()) + 1) * dist.get_world_size()
     all_batches = np.array_split(np.array(all_text), num_batches)
-    rank_batches = all_batches[dist.get_rank():: dist.get_world_size()] 
+    rank_batches = all_batches[dist.get_rank():: dist.get_world_size()]
 
     index_list = np.arange(len(all_text))
     print(len(index_list))
@@ -80,9 +80,9 @@ def main():
 
     # Create main pipe
     pipe = StableDiffusionImg2ImgPipeline.from_pretrained(
-        args.teacher_model_path, 
-        torch_dtype=th.float16 if args.use_fp16 else th.float32, 
-        variant="fp16" if args.use_fp16 else "fp32", 
+        args.teacher_model_path,
+        torch_dtype=th.float16 if args.use_fp16 else th.float32,
+        variant="fp16" if args.use_fp16 else "fp32",
     )
     pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
     if args.scheduler_type == 'DDIM':
@@ -119,7 +119,7 @@ def main():
             )
     else:
         batch_size = args.batch_size
-        
+
     #############
     # Load data #
     #############
@@ -148,16 +148,16 @@ def main():
         )
     else:
         raise(f"Unsupported dataset {args.dataset}...")
-    
+
     eval_pipe.coco_prompts = prepare_coco_prompts(args.coco_path, max_cnt=args.coco_max_cnt)
 
     # load the target and teacher models for distillation, if path specified.
     logger.log(f"loading the teacher model from {args.teacher_model_path}")
     # teacher_model = deepcopy(model).to(dist.dev())
     teacher_model = StableDiffusionImg2ImgPipeline.from_pretrained(
-        args.teacher_model_path, 
-        torch_dtype=th.float16, 
-        variant="fp16", 
+        args.teacher_model_path,
+        torch_dtype=th.float16,
+        variant="fp16",
     ).unet.to(dist.dev())
 
     logger.log("creating the target model")
@@ -169,8 +169,8 @@ def main():
 
     # for dst, src in zip(teacher_model.parameters(), model.parameters()):
     #     assert (dst.data == src.data).all()
-    
-    assert len(list(target_model.buffers())) == len(list(model.buffers())) == len(list(teacher_model.buffers())) == 0 
+
+    assert len(list(target_model.buffers())) == len(list(model.buffers())) == len(list(teacher_model.buffers())) == 0
 
     # Create SD denoiser
     diffusion = DenoiserSD(
@@ -207,11 +207,11 @@ def main():
         coco_ref_stats_path=args.coco_ref_stats_path,
         inception_path=args.inception_path,
         coco_max_cnt=args.coco_max_cnt
-    ).run_loop() #.generate_coco(scheduler_refining=scheduler_refining,
-      #              num_inference_steps=args.steps,
-      #              num_refining_steps=args.refining_steps,
-      #              rollback_value=args.rollback_value,
-      #              scheduler_type=args.scheduler_type)
+    ).generate_coco(scheduler_refining=scheduler_refining,
+                    num_inference_steps=args.steps,
+                    num_refining_steps=args.refining_steps,
+                    rollback_value=args.rollback_value,
+                    scheduler_type=args.scheduler_type)
 
 def create_argparser():
     defaults = dict(
@@ -228,7 +228,7 @@ def create_argparser():
         resume_checkpoint="",
         use_fp16=False,
         fp16_scale_growth=1e-3,
-        dataset='laion', 
+        dataset='laion',
         laion_config="configs/laion.yaml",
         coco_train_path=".",
         coco_path="subset_30k.csv",
@@ -252,3 +252,173 @@ def create_argparser():
 
 if __name__ == "__main__":
     main()
+
+
+    @th.no_grad()
+    def iterative_superres(
+            self,
+            eval_pipe,
+            prompt_embeds,
+            x0_latents,
+            rollback_value=0.3,
+            generator=None,
+            num_inference_steps=50,
+            guidance_scale=8.0,
+            refining_scheduler='DPM',
+    ):
+        patch_size = 8
+        device = eval_pipe._execution_device
+
+        def obtain_orthogonal_matrix():
+            vector = np.asarray([1] * patch_size ** 2)
+            vector = vector / np.linalg.norm(vector)
+            matrix = np.eye(patch_size ** 2)
+            matrix[:, 0] = vector
+            matrix = np.linalg.qr(matrix)[0]
+            if np.sum(matrix[:, 0]) < 0:
+                matrix = -matrix
+            return matrix
+
+        Q = th.from_numpy(obtain_orthogonal_matrix()).to(device).to(th.float32)
+
+        image_size = x0_latents.shape[-1]
+
+        def replacement(x0, x1):
+            x0_flatten = (
+                x0.reshape(-1, 3, image_size, image_size)
+                .reshape(
+                    -1,
+                    3,
+                    image_size // patch_size,
+                    patch_size,
+                    image_size // patch_size,
+                    patch_size,
+                )
+                .permute(0, 1, 2, 4, 3, 5)
+                .reshape(-1, 3, image_size ** 2 // patch_size ** 2, patch_size ** 2)
+            )
+            x1_flatten = (
+                x1.reshape(-1, 3, image_size, image_size)
+                .reshape(
+                    -1,
+                    3,
+                    image_size // patch_size,
+                    patch_size,
+                    image_size // patch_size,
+                    patch_size,
+                )
+                .permute(0, 1, 2, 4, 3, 5)
+                .reshape(-1, 3, image_size ** 2 // patch_size ** 2, patch_size ** 2)
+            )
+            x0 = th.einsum("bcnd,de->bcne", x0_flatten, Q)
+            x1 = th.einsum("bcnd,de->bcne", x1_flatten, Q)
+            x_mix = x0.new_zeros(x0.shape)
+            x_mix[..., 0] = x0[..., 0]
+            x_mix[..., 1:] = x1[..., 1:]
+            x_mix = th.einsum("bcne,de->bcnd", x_mix, Q)
+            x_mix = (
+                x_mix.reshape(
+                    -1,
+                    3,
+                    image_size // patch_size,
+                    image_size // patch_size,
+                    patch_size,
+                    patch_size,
+                )
+                .permute(0, 1, 2, 4, 3, 5)
+                .reshape(-1, 3, image_size, image_size)
+            )
+            return x_mix
+
+        def average_image_patches(x):
+            x_flatten = (
+                x.reshape(-1, 4, image_size, image_size)
+                .reshape(
+                    -1,
+                    4,
+                    image_size // patch_size,
+                    patch_size,
+                    image_size // patch_size,
+                    patch_size,
+                )
+                .permute(0, 1, 2, 4, 3, 5)
+                .reshape(-1, 4, image_size ** 2 // patch_size ** 2, patch_size ** 2)
+            )
+            x_flatten[..., :] = x_flatten.mean(dim=-1, keepdim=True)
+            return (
+                x_flatten.reshape(
+                    -1,
+                    4,
+                    image_size // patch_size,
+                    image_size // patch_size,
+                    patch_size,
+                    patch_size,
+                )
+                .permute(0, 1, 2, 4, 3, 5)
+                .reshape(-1, 4, image_size, image_size)
+            )
+
+        do_classifier_free_guidance = guidance_scale > 1.0
+        # 4. Sample timesteps uniformly (first step is 981)
+        self.scheduler.alphas_cumprod = self.scheduler.alphas_cumprod.to(device)
+        # 4. Sample timesteps uniformly (first step is 981)
+        timesteps = torch.linspace(int(rollback_value * 1000), 1, steps=num_inference_steps + 1,
+                                   dtype=int, device=device)
+        assert len(timesteps) == num_inference_steps + 1
+
+        # 5. Prepare latent variables
+        rollback_timestep = torch.tensor([int(rollback_value * 1000)], device=device)
+        sqrt_alpha_prod = self.scheduler.alphas_cumprod[rollback_timestep] ** 0.5
+        sqrt_one_minus_alpha_prod = (1 - self.scheduler.alphas_cumprod[rollback_timestep]) ** 0.5
+
+        noise = torch.randn(x0_latents.size(), generator=generator, device=device)
+        latents = sqrt_alpha_prod * x0_latents + sqrt_one_minus_alpha_prod * noise
+        images = average_image_patches(x0_latents)
+
+        eval_pipe.scheduler.timesteps = timesteps
+        eval_pipe.scheduler.num_inference_steps = len(timesteps)
+
+        for i, t in enumerate(timesteps):
+            # expand the latents if we are doing classifier free guidance
+            latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
+            latent_model_input = eval_pipe.scheduler.scale_model_input(latent_model_input, t)
+
+            # predict the noise residual
+            noise_pred = eval_pipe.unet(
+                latent_model_input,
+                t,
+                encoder_hidden_states=prompt_embeds,
+                cross_attention_kwargs=None,
+                return_dict=False,
+            )[0]
+
+            # perform guidance
+            if do_classifier_free_guidance:
+                noise_pred_uncond, noise_pred_text = noise_pred.chunk(2)
+                noise_pred = noise_pred_uncond + guidance_scale * (noise_pred_text - noise_pred_uncond)
+
+            if i == len(timesteps) - 1:
+                t2 = torch.zeros_like(timesteps[i])
+            else:
+                t2 = timesteps[i + 1]
+
+            x0 = self.denoise(noise_pred, t, latents)
+            sqrt_alpha_prod = self.scheduler.alphas_cumprod[t2] ** 0.5
+            sqrt_one_minus_alpha_prod = (1 - self.scheduler.alphas_cumprod[t2]) ** 0.5
+            x0 = th.clamp(x0, -1.0, 1.0)
+            x0 = replacement(images, x0)
+
+            sqrt_alpha_prod = self.scheduler.alphas_cumprod[t2] ** 0.5
+            sqrt_one_minus_alpha_prod = (1 - self.scheduler.alphas_cumprod[t2]) ** 0.5
+
+            noise = torch.randn(latents.size(), device=device)
+            latents = sqrt_alpha_prod * x0 + sqrt_one_minus_alpha_prod * noise
+
+        image = eval_pipe.vae.decode(latents / eval_pipe.vae.config.scaling_factor, return_dict=False)[0]
+        do_denormalize = [True] * image.shape[0]
+        image = eval_pipe.image_processor.postprocess(image, output_type="pil", do_denormalize=do_denormalize)
+
+        image_loh = eval_pipe.vae.decode(image / eval_pipe.vae.config.scaling_factor, return_dict=False)[0]
+        do_denormalize = [True] * image_loh.shape[0]
+        image_loh = eval_pipe.image_processor.postprocess(image_loh, output_type="pil", do_denormalize=do_denormalize)
+        return image, image_loh
