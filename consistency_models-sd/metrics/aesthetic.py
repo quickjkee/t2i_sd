ad6c39d1aa89490eb11b0ba591695de82feca0dc
quickjkee <askjfasokf@askdjasoi.ee>
2023-10-28 18:21:20 +0300

    first

--- consistency_models-sd/metrics/aesthetic.py
+++ consistency_models-sd/metrics/aesthetic.py
@@ -136,7 +136,7 @@ def calculate_reward_given_paths(path_images, path_prompts):
 
 @torch.no_grad()
 def calculate_clip_given_paths(path_images, path_prompts):
-    model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
+    model, _, preprocess = open_clip.create_model_and_transforms('ViT-G-14', pretrained='laion2b_s34b_b79k')
     tokenizer = open_clip.get_tokenizer('ViT-B-32')
     df = pd.read_csv(path_prompts)
     all_text = list(df['caption'])
