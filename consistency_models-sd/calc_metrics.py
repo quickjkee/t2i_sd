6e23f222b920192fd7f6ae6c6c395592738a3ab3
quickjkee <askjfasokf@askdjasoi.ee>
2023-10-28 11:54:15 +0300

    first

--- consistency_models-sd/calc_metrics.py
+++ consistency_models-sd/calc_metrics.py
@@ -74,4 +74,4 @@ else:
 
     for perc in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]:
         folder_adaptive = mover(folder_proxy, reward_proxy, folder, percentile=perc)
-        reward = calculate_clip_given_paths(f'{folder_adaptive}/', folder_csv)
+        reward = calculate_clip_given_paths(f'{folder_adaptive}', folder_csv)
